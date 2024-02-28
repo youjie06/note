028c68c1606b3f667291a20f452a6423298896f7
YING0512 <5B0G0006@stust.edu.tw>
2024-02-28 18:28:16 +0800

    設定視窗長寬最小值

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 
+#!視窗設定
 win = tk.Tk()       #建立主視窗
 win.title("Note")   #視窗標題
-win.geometry("400*200")
+win.geometry('1024x768') #設定視窗長寬('寬x長') > x小寫英文
+win.minsize(width=1024,height=768)#設定視窗範圍最小值
+
+
 win.mainloop()      #常駐主視窗
